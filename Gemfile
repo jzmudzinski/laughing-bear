01aed1dc10eb8b8419f2349606d3ba2da2ed1a21
Jarek Zmudzinski <jarek@toronegro.net>
2013-05-17 08:44:24 +0200

    init

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'rails-api'
+
+gem 'jruby-openssl'
+
+# DB SUPPORT
+# sqlite3
+# gem 'activerecord-jdbcsqlite3-adapter'
+
+# To use Jbuilder templates for JSON
+# gem 'jbuilder'
+
+# Deploy with Capistrano
+# gem 'capistrano', :group => :development
+
+gem 'torquebox', "2.3.1"
+
+group :development, :test do
+  gem 'sdoc'
+  gem 'rspec-rails'
+end
+
+group :development do
+  gem 'capistrano'
+  gem 'torquebox-capistrano-support'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_toke to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+RailsServiceTemplate::Application.config.secret_token = '1535559b125261bbf123e52326ad3d048490277aaff448b5c34ca7b5ef34c9f2f6390716ff18f3d5975482a8ee74c91ef143d1694261b57b693cceef5c5144e9'
